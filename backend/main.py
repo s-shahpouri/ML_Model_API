9a459420c8632db8260cd45ab8cecb995aeae4bb
s-shahpouri <z.shahpouri@gmail.com>
2024-11-21 08:33:29 +0100

    Successfully lunched my first API

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,12 @@ import uuid
 import cv2
 import uvicorn
 from fastapi import File
-from fasapi import FastAPI
+from fastapi import FastAPI
 from fastapi import UploadFile
-import numpy
+import numpy as np
 from PIL import Image
 import config
-import imference
+import inference
 
 
 app = FastAPI()
@@ -19,7 +19,13 @@ def read_root():
 
 @app.post("/{style}")
 def get_image(style: str, file: UploadFile = File(...)):
-    pass
+    image = np.array(Image.open(file.file))
+    model = config.STYLE[style]
+    output, resized = inference.inference(model,image)
+    name = f"/storage/{str(uuid.uuid())}.jpg"
+    cv2.imwrite(name, output)
+    return {"name": name}
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "0.0.0.0", post = 8080)
+    uvicorn.run("main:app", host = "0.0.0.0", port = 8080)
